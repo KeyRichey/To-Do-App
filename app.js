16712684c84bc3ccded55936860db394ac7cffc2
Keyona Richey <keyona.richey@gmail.com>
2018-12-09 19:03:18 -0800

    Assignment 5 completed

--- app.js
+++ app.js
@@ -3,27 +3,38 @@ function onReady() {
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
 
- addToDoForm.addEventListener('submit', event => {
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-   // get the text
-  let title = newToDoText.value;
+    // get the text
+    let title = newToDoText.value;
     // create a new li
-   let newLi = document.createElement('li');
-   // create a new input
-   let checkbox = document.createElement('input');
-   // set the input's type to checkbox
-   checkbox.type = "checkbox";
-   // set the title
+    let newLi = document.createElement('li');
+    // create a new input
+    let checkbox = document.createElement('input');
+    // set the input's type to checkbox
+    checkbox.type = "checkbox";
+    // create a delete button
+    let deleteButton = document.createElement('button');
+    // set text of the element
+    deleteButton.textContent = "Delete";
+    // set the title
     newLi.textContent = title;
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
     // attach the li to the ul
     toDoList.appendChild(newLi);
     //empty the input
-   newToDoText.value = '';
- });
+    newToDoText.value = '';
+    // attach delete button
+    toDoList.appendChild(deleteButton);
+    // delete button event
+    deleteButton.addEventListener('click', event => {
+      toDoList.removeChild(newLi);
+      toDoList.removeChild(deleteButton);
+    });
+  });
 }
 
 window.onload = function(){
-   onReady();
+  onReady();
 };
